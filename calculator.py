71a161ccd6c3c4ee6aa6f9143846d06cb893b42f
Panupong Wongjinda <panupong.wongjinda@gmail.com>
2025-05-14 18:30:49 +0700

    add Panupong Wongjinda

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ GUI = Tk()
 GUI.title('Basic Git & GitHub by Uncle Engineer')
 GUI.geometry('500x700')
 
-L = Label(GUI,text='loong Wissawakorn')
+L = Label(GUI,text='Panupong Wongjinda')
 L.pack()
 L = Label(GUI,text='panupong2548')
 L.pack()
