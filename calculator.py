ae3b8e36cdcbec4e0135c26a11fcd03f1ef40cda
Panupong Wongjinda <panupong.wongjinda@gmail.com>
2025-05-14 17:47:18 +0700

    add calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk,messagebox
+
+GUI = Tk()
+GUI.title('Basic Git & GitHub by Uncle Engineer')
+GUI.geometry('500x700')
+
+L = Label(GUI,text='loong Wissawakorn')
+L.pack()
+L = Label(GUI,text='panupong2548')
+L.pack()
+
+GUI.mainloop()
