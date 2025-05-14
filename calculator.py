e3bae389a2c54252fccb04ba4ca56f3aea989d2e
Panupong Wongjinda <panupong.wongjinda@gmail.com>
2025-05-14 19:20:36 +0700

    add calculator function

--- calculator.py
+++ calculator.py
@@ -12,4 +12,22 @@ L.pack()
 L = Label(GUI,text='gamezee00008')
 L.pack()
 
+v1 = StringVar()
+E1 = ttk.Entry(GUI,textvariable=v1)
+E1.pack()
+
+v2 = StringVar()
+E2 = ttk.Entry(GUI,textvariable=v2)
+E2.pack()
+
+def cal():
+    c = float(v1.get()) * float(v2.get())
+    r1 = v1.get()
+    r2 = v2.get()
+    text = f'{r1}x{r2}={c}'
+    messagebox.showinfo('Result',text)
+
+b1 = ttk.Button(GUI,text='Calculate Now!',command=cal)
+b1.pack()
+
 GUI.mainloop()
